106f066690ab434ebe9185ad692d93880ce184cd
Samuel Lim <sclg9w@mail.umkc.edu>
2020-03-02 23:28:38 -0600

    Added basic server logic and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,19 @@
+#[macro_use]
+extern crate rouille;
+
+use rouille::{
+    Request,
+    Response,
+};
+
 fn main() {
+    rouille::start_server("localhost:8080", move |req| {
+        // A simple test
+        router!(req,
+            (GET) (/) => {
+                Response::text("Hello! Unfortunately there is nothing to see here.")
+            },
+            _ => Response::empty_404()
+        )
+    });
 }
